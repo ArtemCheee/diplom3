09fe830b8456976220a43a41a2af10c89e3fc616
Chernoschekov Artem <solo16394@mail.ru>
2026-06-26 20:59:52 +0600

    Локаторы кнопок клик и проверка видимости кнопки оформить заказ появляющейсся после авторизации

--- src/main/java/page/MainPage.java
+++ src/main/java/page/MainPage.java
@@ -3,19 +3,33 @@ package page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.util.NoSuchElementException;
+
 
 public class MainPage {
 
     private final WebDriver webDriver;
     private final By PersonalAccountButton = By.xpath("/html/body/div/div/header/nav/a");
 
+    private final By mainPageLoginButton = By.xpath("//button[text()='Войти в аккаунт']");
+
+    private final By makeOrderButton = By.xpath("//button[text()='Оформить заказ']");
+
 
     public MainPage(WebDriver webDriver) {
         this.webDriver = webDriver;
     }
 
-    public void ckickPersonalAccountButton(){
+    public void clickPersonalAccountButton(){
       webDriver.findElement(this.PersonalAccountButton).click();
     }
 
+    public void clickMainPageLoginButton(){
+        webDriver.findElement(mainPageLoginButton).click();
+    }
+
+    public boolean isMakeOrderButtonDisplayed(){
+
+        return webDriver.findElement(makeOrderButton).isDisplayed();
+    }
     }
