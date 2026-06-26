0aa4f5e159dcad6d083aaef04b8ce3ed699e9ec2
Chernoschekov Artem <solo16394@mail.ru>
2026-06-26 19:26:09 +0600

    данные пользователя генерим фейкером

---
+++ src/main/java/ClientData.java
@@ -0,0 +1,25 @@
+import com.github.javafaker.Faker;
+
+
+    public class ClientData {
+
+        static Faker user = new Faker();
+
+        public static String generateRandomEmail() {
+            return user.regexify("[a-zA-Z]{5,10}") + System.currentTimeMillis() + "@ya.ru";
+        }
+
+        public static String generateRandomPassword() {
+            return user.regexify("[A-Za-z0-9]{8}");
+        }
+
+        public static String generateRandomName() {
+            return user.regexify("[A-Za-z]{8}");
+
+        }
+
+        public static String generateWrongRandomPassword() {
+            return user.regexify("[0-9]{2}");
+        }
+
+    }
