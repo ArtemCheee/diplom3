dde18a90755de0440e668822ca71bceb8741ff2f
Chernoschekov Artem <solo16394@mail.ru>
2026-06-27 13:37:59 +0600

    добавил вариативность окружения, по умолчанию google chrome, если USE_YANDEX изменить на true и в пути прописать адрес браузера - запустится yandex browser

--- src/test/java/BaseUiTest.java
+++ src/test/java/BaseUiTest.java
@@ -3,16 +3,28 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseUiTest {
 
     protected WebDriver webDriver;
     public final static String MAIN_PAGE = "https://stellarburgers.education-services.ru/";
 
+    private static final boolean USE_YANDEX = false;
+
+
     @Before
-    public void startBrowser(){
+
+    public void startBrowser() {
         WebDriverManager.chromedriver().setup();
-        this.webDriver = new ChromeDriver();
+
+        if (USE_YANDEX) {
+            ChromeOptions options = new ChromeOptions();
+            options.setBinary("путь к яндекс браузеру и USE_YANDEX меняем на true");
+            this.webDriver = new ChromeDriver(options);
+        } else {
+            this.webDriver = new ChromeDriver();
+        }
         this.webDriver.get(MAIN_PAGE);
     }
 
