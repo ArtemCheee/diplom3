644a2c8432c1598d14957e40a44975ab4d298327
Chernoschekov Artem <solo16394@mail.ru>
2026-06-27 20:26:50 +0600

    вынес создание обьектов в efore, создание клиента через api и настройки ожидания

--- src/test/java/BaseUiTest.java
+++ src/test/java/BaseUiTest.java
@@ -1,11 +1,20 @@
 import UserData.ClientModel;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.restassured.response.Response;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import page.LoginPage;
+import page.MainPage;
+import page.SignUpPage;
 
+import java.time.Duration;
+
+import static UserData.ClientData.generateRandomUser;
+import static UserData.ClientSteps.createClient;
 import static UserData.ClientSteps.deleteClient;
 
 public class BaseUiTest {
@@ -16,10 +25,17 @@ public class BaseUiTest {
     private static final boolean USE_YANDEX = false;
 
     protected String accessToken;
+    protected Response clientResponse;
+    protected ClientModel client;
+    protected MainPage mainPage;
+    protected LoginPage loginPage;
+    protected SignUpPage signUpPage;
+    protected WebDriverWait wait;
 
     @Before
 
     public void startBrowser() {
+
         WebDriverManager.chromedriver().setup();
 
         if (USE_YANDEX) {
@@ -29,9 +45,29 @@ public class BaseUiTest {
         } else {
             this.webDriver = new ChromeDriver();
         }
+        this.webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        this.webDriver.manage().window().maximize();
+
         this.webDriver.get(MAIN_PAGE);
+
+        wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+
+        mainPage = new MainPage(webDriver);
+        loginPage = new LoginPage(webDriver);
+        signUpPage = new SignUpPage(webDriver);
     }
 
+    protected void createClientBefore() {
+        client = generateRandomUser();
+
+        clientResponse = createClient(client)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().response();
+
+        accessToken = clientResponse.jsonPath().getString("accessToken");
+    }
 
     @After
     public void deleteClientAfterTest() {
